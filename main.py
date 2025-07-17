62c9ead720cfcb1d4eddd63f113964d0b29a4a7b
tenmo <tenmo2003@gmail.com>
2025-07-17 10:40:18 +0700

    add flag to separate between 2 types and reformat message

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
+from datetime import datetime, timedelta, timezone
+import argparse
+
 from services.fetcher import fetch_domestic_gold_prices, fetch_international_gold_prices
 from services.formatter import format_domestic_data_as_code_block, format_international_data
 from services.telegram_bot import send_to_telegram
+from utils.day_converter import convert_day_to_vietnamese
 
-if __name__ == "__main__":
+def main(data_type):
     print("Starting gold price bot...")
 
-    buy_trend, data = fetch_domestic_gold_prices()
     message = ""
 
-    if data:
-        message += format_domestic_data_as_code_block(data, buy_trend)
-    else:
-        print(buy_trend)
+    if data_type in ("domestic", "both"):
+        buy_trend, data = fetch_domestic_gold_prices()
+        if data:
+            message += format_domestic_data_as_code_block(data, buy_trend)
+        else:
+            print(buy_trend)
 
-    current_price, change = fetch_international_gold_prices()
-    if change:
-        message += format_international_data(current_price, change)
-    else:
-        print(current_price)
+    if data_type in ("international", "both"):
+        current_price, change = fetch_international_gold_prices()
+        if message:
+            message += "\n\n"
+        if change:
+            message += format_international_data(current_price, change)
+        else:
+            print(current_price)
+
+    # TODO: Add Bảo tín minh châu table
 
     if message:
+        now = datetime.now(timezone.utc) + timedelta(hours=7)
+        current_time = now.strftime("%H:%M:%S")
+        current_day = convert_day_to_vietnamese(now.strftime("%A"))
+        current_date = now.strftime("%d/%m/%Y")
+        message = "```" + f"{current_time}\n" + f"{current_day} {current_date}\n" + message + "```"
         send_to_telegram(message)
 
     print("Gold price bot finished.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Gold Price Bot")
+    parser.add_argument(
+        "--type",
+        choices=["domestic", "international", "both"],
+        default="both",
+        help="Type of gold data to fetch (default: both)",
+    )
+
+    args = parser.parse_args()
+    main(args.type)
--- services/formatter.py
+++ services/formatter.py
@@ -1,15 +1,8 @@
-from datetime import datetime, timedelta, timezone
-from utils.day_converter import convert_day_to_vietnamese
-
 def format_domestic_data_as_code_block(data, buy_trend):
     """
     Format danh sách dữ liệu giá vàng dưới dạng bảng (code block).
     """
     print("Formatting data as code block...")
-    now = datetime.now(timezone.utc) + timedelta(hours=7)
-    current_time = now.strftime("%H:%M:%S")
-    current_day = convert_day_to_vietnamese(now.strftime("%A"))
-    current_date = now.strftime("%d/%m/%Y")
 
     header = ["Loại", "Mua", "Bán"]
     line = "+------+--------------+--------------+"
@@ -21,8 +14,6 @@ def format_domestic_data_as_code_block(data, buy_trend):
         emoji = "⚪"
 
     table = [
-        f"{current_time}",
-        f"{current_day} {current_date}",
         f"Giá vàng trong nước: {emoji}",
         "",
         line,
@@ -40,7 +31,7 @@ def format_domestic_data_as_code_block(data, buy_trend):
     table.append(line)
 
     print("Data formatted successfully.")
-    return "```" + "\n".join(table) + "\n```"
+    return "\n".join(table)
 
 def format_international_data(current_price, change):
     def escape(text):
@@ -58,10 +49,8 @@ def format_international_data(current_price, change):
         emoji = "⚪"
 
     message = (
-            "```\n"
             "Giá vàng quốc tế (USD): \n\n"
             f"{escaped_price}\n"
             f"{escaped_change} {emoji}"
-            "```"
     )
     return message
